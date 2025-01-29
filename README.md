883f2a1843faee1554551ad6d78d90c36fafc616
Joey Saladino <joeysaladino@gmail.com>
2025-01-28 21:39:41 -0500

    Adding Windows script

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 
 
 ## 🛠️ Installation
-This script makes it easy to install tDF, pfQuest, pfQuest-turtle, SuperAPI, and SuperWoW on Linux
+This script makes it easy to install tDF, pfQuest, pfQuest-turtle, SuperAPI, and SuperWoW on Linux and Windows
 
+### Linux
 1. Install twow from Lutris
 2. Download this script
 3. From a Terminal: 
@@ -14,5 +15,14 @@ This script makes it easy to install tDF, pfQuest, pfQuest-turtle, SuperAPI, and
     3. Point Lutris to `SuperWoWlauncher.exe`
     4. Start Turtle WoW and enjoy
 
-## 💖 Donate
-https://www.paypal.com/donate/?hosted_button_id=WPTX2BMBARSG2
+### Windows
+1. Download and install Turtle WoW from [turtle-wow.org](https://turtle-wow.org/)
+2. Install [Git for Windows](https://git-scm.com/downloads/win)
+3. Download this script
+4. Update the `$TWOW_DIR` variable in `TWOW-Setup.ps1` to point to the location where you installed Turtle WoW (where WoW.exe is stored)
+5. From an Administrator PowerShell prompt or Windows Terminal:
+    1. Type `./TWOW-Setup.ps1`
+    2. Create or update a shortcut to `SuperWoWlauncher.exe` wherver you'd like (on your desktop for example)
+    2. Start Turtle WoW from the newly created shortcut and enjoy
+
+> **_NOTE:_** SuperWoW is flagged as a Trojan by Windows Defender. This is a false positive as stated on [the github issue](https://github.com/balakethelock/SuperWoW/issues/20#issuecomment-2186479946). Therefore, this script creates a Windows Defender exception for the Turtle WoW directory before downloading SuperWoW.
---
+++ TWOW-Setup.ps1
@@ -0,0 +1,91 @@
+﻿$ErrorActionPreference = "Stop"
+$env:GIT_REDIRECT_STDERR = '2>&1'
+
+# Check if the script is running as an administrator
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # Relaunch the script with elevated privileges
+    $newProcess = Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs -PassThru
+    # Exit the current non-elevated session
+    exit
+}
+
+# Define TWOW Directory
+$TWOW_DIR = "D:\Games\Turtlewow"
+
+# Define AddOns Directory
+$ADDONS_DIR = Join-Path $TWOW_DIR "Interface\AddOns"
+
+# Define an array of directory paths
+$directories = @(
+    "$ADDONS_DIR\pfQuest",
+    "$ADDONS_DIR\pfQuest-turtle",
+    "$ADDONS_DIR\SuperAPI",
+    "$ADDONS_DIR\Turtle-Dragonflight",
+    "$TWOW_DIR\Interface\Minimap",
+    "$TWOW_DIR\Interface\TargetingFrame"
+)
+
+# Loop through each directory in the array
+foreach ($dir in $directories) {
+    # Check if the directory exists
+    if (Test-Path -Path $dir) {
+        # Remove the directory and its contents
+        Remove-Item -Path $dir -Recurse -Force
+        Write-Host "Removed directory: $dir"
+    } else {
+        Write-Host "Directory does not exist: $dir"
+    }
+}
+
+# Navigate to the AddOns directory
+Set-Location -Path $ADDONS_DIR
+
+# Clone the repositories
+git clone https://github.com/TheLinuxITGuy/Turtle-Dragonflight.git
+git clone https://github.com/balakethelock/SuperAPI.git
+git clone https://github.com/shagu/pfQuest.git
+git clone https://github.com/shagu/pfQuest-turtle.git
+
+# Move Minimap and TargetingFrame into the Interface folder
+Move-Item -Path "$ADDONS_DIR\Turtle-Dragonflight\Interface\Minimap" -Destination "$TWOW_DIR\Interface" -Force
+Move-Item -Path "$ADDONS_DIR\Turtle-Dragonflight\Interface\TargetingFrame" -Destination "$TWOW_DIR\Interface" -Force
+
+# Navigate to the TurtleWoW directory
+Set-Location -Path $TWOW_DIR
+
+# Define an array of SuperWoW files
+$superWoW = @(
+    "$TWOW_DIR\SuperWoWhook.dll",
+    "$TWOW_DIR\SuperWoWlauncher.exe"
+)
+
+# Loop through each file in the array
+foreach ($item in $superWoW) {
+    # Check if the SuperWoW files exist
+    if (Test-Path $item) {
+        # Remove the file
+        Remove-Item $item -Force
+        Write-Host "Removed: $item"
+    } else {
+        Write-Host "File does not exist: $item"
+    }
+}
+
+# Add a Windows Defender exclusion for SuperWoW
+try {
+    Add-MpPreference -ExclusionPath $TWOW_DIR
+    Write-Host "Successfully added exclusion path: $TWOW_DIR"
+    Start-Sleep -Seconds 5
+} catch {
+    Write-Host "Failed to add exclusion path: $TWOW_DIR"
+    Write-Host "Error: $_"
+}
+
+# Download the SuperWoW release
+Invoke-WebRequest -Uri "https://github.com/balakethelock/SuperWoW/releases/download/Release/SuperWoW.release.1.3.zip" -OutFile "$TWOW_DIR\SuperWoW.release.1.3.zip"
+
+# Extract the zip file
+Expand-Archive -Path "$TWOW_DIR\SuperWoW.release.1.3.zip" -DestinationPath $TWOW_DIR -Force
+
+# Remove the zip file
+Remove-Item -Path "$TWOW_DIR\SuperWoW.release.1.3.zip" -Force
